f2240c74002e9b7b440db7654a8578e00422bb9d
appelm <appelm91@protonmail.com>
2017-04-28 15:01:10 -0500

    Updates
    1. Changed from restful style to parameters in the requests.
    2. Added support for 'pug' (formerly jade)

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+const pug = require('pug');
 
+// 'view engine' is an Express.js system variable. So here we are setting the value of that system variable to 'pug' so we can use pug in our routes. 
+app.set('view engine', 'pug')
+// 'port' is an Express.js system variable
 app.set('port', process.env.PORT || 3000);
 
 //These are our routes... Each of these JS files contains one of our pages!
@@ -8,7 +12,9 @@ app.use(require('./routes/index'));
 app.use(require('./routes/about'));
 
 
-// Creating the server and listening on port the 
+// Creating the server and listening on port specified by the windows 'port' envitonment variable
+// 'set port=443'
+// 'node app.js'
 var server = app.listen(app.get('port'), function() {
     console.log('Listening on port ' + app.get('port'));
 });
